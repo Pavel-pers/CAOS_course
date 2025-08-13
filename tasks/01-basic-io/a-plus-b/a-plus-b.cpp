286a3eea2b2c7ab559b8200f239e463024735995
Oleg Shatov <oleq.shatov@yandex.ru>
2025-08-13 06:16:45 +0000

    Public files export

--- tasks/01-basic-io/a-plus-b/a-plus-b.cpp
+++ tasks/01-basic-io/a-plus-b/a-plus-b.cpp
@@ -1,7 +1,8 @@
 #include "a-plus-b.h"
 
 uint32_t Sum(uint32_t a, uint32_t b) {
-    (void)(a, b); // TODO: remove before flight.
+    (void)a; // TODO: remove before flight.
+    (void)b; // TODO: remove before flight.
     // TODO: your code here.
     throw "TODO";
 }
