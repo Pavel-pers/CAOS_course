358b70eaa8e0e51fe8eb25a09b65dba3f65a2f37
Pavel Turaev <pnturaev@edu.hse.ru>
2025-09-19 18:01:59 +0300

    format code

--- tasks/01-basic-io/reverse-list/reverse-list.cpp
+++ tasks/01-basic-io/reverse-list/reverse-list.cpp
@@ -1,10 +1,10 @@
- #include "reverse-list.hpp"
+#include "reverse-list.hpp"
 
 ListNode* Reverse(ListNode* node) {
-    ListNode *prev = nullptr;
-    ListNode *cur = node;
+    ListNode* prev = nullptr;
+    ListNode* cur = node;
     while (cur != nullptr) {
-        ListNode *next = cur->next;
+        ListNode* next = cur->next;
         cur->next = prev;
         prev = cur;
         cur = next;
