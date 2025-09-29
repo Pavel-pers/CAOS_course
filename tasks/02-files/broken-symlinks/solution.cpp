e7a0569a911e8bc4d10fe283556cffdb0d3faba9
Pavel Turaev rsb-pi <p.pers798@gmail.com>
2025-09-29 09:58:45 +0100

    solve problem

--- tasks/02-files/broken-symlinks/solution.cpp
+++ tasks/02-files/broken-symlinks/solution.cpp
@@ -2,11 +2,34 @@
 
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <unistd.h>
 
-#include <unused.hpp>  // TODO: remove before flight.
+bool isFileExists(const char* path) {
+    struct stat buffer {};
+
+    return (stat(path, &buffer) == 0);
+}
+
+bool isBrokenLink(const char* path) {
+    struct stat link_stat {};
+
+    if (lstat(path, &link_stat) != 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 int main(int argc, const char* argv[]) {
-    UNUSED(argc, argv);  // TODO: remove before flight.
-    // TODO: your code here.
+    for (int i = 1; i < argc; ++i) {
+        if (!isFileExists(argv[i])) {
+            if (isBrokenLink(argv[i])) {
+                std::cout << argv[i] << " (missing)";
+            } else {
+                std::cout << argv[i] << " (broken symlink)";
+            }
+        } else {
+            std::cout << argv[i];
+        }
+        std::cout << '\n';
+    }
 }
