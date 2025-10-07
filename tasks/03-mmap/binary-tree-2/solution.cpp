c4229cb83a6ecf25726d3edc38f018778eeb996b
Pavel Turaev rsb-pi <p.pers798@gmail.com>
2025-10-07 21:00:25 +0100

    solve problem

--- tasks/03-mmap/binary-tree-2/solution.cpp
+++ tasks/03-mmap/binary-tree-2/solution.cpp
@@ -1,6 +1,9 @@
 #include <cstdint>
-
-#include <unused.hpp>  // TODO: remove before flight.
+#include <iostream>
+#include <sys/fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 struct Node {
     int32_t key;
@@ -8,6 +11,53 @@ struct Node {
     int32_t right_idx;
 };
 
+void dfs(int32_t idx, const Node* buf) {
+    Node cur_node = buf[idx];
+    if (cur_node.right_idx != 0) {
+        dfs(cur_node.right_idx, buf);
+    }
+    std::cout << cur_node.key << ' ';
+    if (cur_node.left_idx != 0) {
+        dfs(cur_node.left_idx, buf);
+    }
+}
+
 int main(int argc, char* argv[]) {
-    UNUSED(argc, argv);  // TODO: remove before flight.
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <file>" << std::endl;
+        return 1;
+    }
+
+    const ssize_t fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        std::cerr << "Failed to open file " << argv[1] << std::endl;
+        return 1;
+    }
+
+    struct stat st {};
+
+    if (fstat(fd, &st) == -1) {
+        std::cerr << "Failed to stat file " << argv[1] << std::endl;
+        return 1;
+    }
+
+    void* buf = mmap(nullptr, st.st_size, PROT_READ, MAP_PRIVATE,
+                     static_cast<int>(fd), 0);
+    if (buf == MAP_FAILED) {
+        std::cerr << "Failed to map file " << argv[1] << std::endl;
+        return 1;
+    }
+
+    const auto data = static_cast<const Node*>(buf);
+    dfs(0, data);
+
+    if (munmap(buf, st.st_size) == -1) {
+        std::cerr << "Failed to unmap file " << argv[1] << std::endl;
+        return 1;
+    }
+
+    if (close(fd) == -1) {
+        std::cerr << "Failed to close file " << argv[1] << std::endl;
+        return 1;
+    }
 }
