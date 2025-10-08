873c82887d9ae5fec83d99eb8a1299948e79e530
Pavel Turaev rsb-pi <p.pers798@gmail.com>
2025-10-08 16:59:20 +0100

    solve problem

--- tasks/03-mmap/simple-allocator/simple-allocator.cpp
+++ tasks/03-mmap/simple-allocator/simple-allocator.cpp
@@ -3,21 +3,52 @@
 #include <syscalls.hpp>
 
 #include <cstddef>
+#include <sys/mman.h>
 #include <type_traits>
 
-struct AllocatorState {
-    static constexpr size_t kBlockSize = 16;
-    static constexpr size_t kPageSize = 1 << 12;
-
+class AllocatorState {
+  public:
     void* Allocate16() {
-        return nullptr;  // TODO: remove before flight.
+        if (cur_free) {
+            void* cur_ptr = cur_free;
+            cur_free = next_free;
+            return static_cast<void*>(cur_ptr);
+        }
+
+        if (cur_page && used + kBlockSize <= kPageSize) {
+            char* ptr = cur_page + used;
+            used += kBlockSize;
+            return ptr;
+        } else {
+            return make_new_page();
+        }
     }
 
     void Deallocate(void* ptr) {
-        (void)ptr;  // TODO: remove before flight.
+        next_free = cur_free;
+        cur_free = ptr;
     }
 
-    // TODO: your code here.
+  private:
+    void* make_new_page() {
+        void* page = MMap(nullptr, kPageSize, PROT_READ | PROT_WRITE,
+                          MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+        if (page == MAP_FAILED) {
+            return nullptr;
+        }
+
+        cur_page = static_cast<char*>(page);
+        used = kBlockSize;
+        return cur_page;
+    }
+
+    static constexpr size_t kBlockSize = 16;
+    static constexpr size_t kPageSize = 1 << 12;
+
+    void* cur_free;
+    void* next_free;
+    char* cur_page;
+    size_t used;
 };
 
 static AllocatorState allocator_state_;
