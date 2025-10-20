4b94ecceec72e4754d7e04f72b248dd2c7b966f6
Pavel Turaev <pnturaev@edu.hse.ru>
2025-10-20 20:33:40 +0300

    solve goto

--- tasks/05-misc/goto/count-substrings.cpp
+++ tasks/05-misc/goto/count-substrings.cpp
@@ -1,8 +1,56 @@
 #include "count-substrings.h"
 
-#include <unused.hpp>  // TODO: remove before flight.
-
 SubstringsCount CountSubstrings(const char* s) {
-    UNUSED(s);  // TODO: remove before flight.
-    return {};  // TODO: remove before flight.
+    SubstringsCount res{};
+    if (s == nullptr) {
+        return res;
+    }
+
+    const char* it = s;
+    char ch = '\0';
+
+    goto S0;
+
+S0:
+    ch = *it++;
+    if (ch == '\0') goto END;
+    if (ch == 'o') goto So;
+    if (ch == 'r') goto Sr;
+    goto S0;
+
+So:
+    ch = *it++;
+    if (ch == '\0') goto END;
+    if (ch == 's') {
+        ++res.os_num;
+        goto S0;
+    }
+    if (ch == 'o') goto So;
+    if (ch == 'r') goto Sr;
+    goto S0;
+
+Sr:
+    ch = *it++;
+    if (ch == '\0') goto END;
+    if (ch == 'o') goto Sro;
+    if (ch == 'r') goto Sr;
+    goto S0;
+
+Sro:
+    ch = *it++;
+    if (ch == '\0') goto END;
+    if (ch == 'p') {
+        ++res.rop_num;
+        goto S0;
+    }
+    if (ch == 's') {
+        ++res.os_num;
+        goto S0;
+    }
+    if (ch == 'o') goto So;
+    if (ch == 'r') goto Sr;
+    goto S0;
+
+END:
+    return res;
 }
